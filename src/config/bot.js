c1642e4b0a0ab818884bf515e0339d28e651b135
Gab0089 <gustav0089pc@gmail.com>
2026-07-02 11:23:46 -0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "leite nelas",
+        name: "𝐥𝐞𝐢𝐭𝐞 𝐧𝐞𝐥𝐚𝐬",
         // Activity type number (0 = Playing).
         type: 0,
       },
